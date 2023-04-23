2ce6219004b7587491e0c8530b4ba0882ba6c56c
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-04-23 16:16:48 -0300

    cev-javascript aula11

---
+++ cev-javascript/aula-11(condicoes-p1)/ex008(velocidade-carro)/main.js
@@ -0,0 +1,9 @@
+var velo = 50.5
+
+console.log(`velocidade do seu carro: ${velo} km/h`)
+
+if (velo >= 60){
+    console.log("velocidade excedida! MULTADO")
+} else {
+    console.log("velocidade normal, pode passar!")
+}
---
+++ cev-javascript/aula-11(condicoes-p1)/ex009(pais)/main.js
@@ -0,0 +1,8 @@
+var pais = "Canada"
+
+console.log(`Você vive em: ${pais}`)
+if(pais == "Brasil") {
+    console.log("Você é Brasileiro")
+} else {
+    console.log("Você é extrangeiro")
+}
