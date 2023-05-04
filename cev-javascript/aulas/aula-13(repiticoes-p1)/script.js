11edeab73d003b1b88e91306c2128878e8ff9f62
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-04 00:04:22 -0300

    cev-javascript-aula-13

---
+++ cev-javascript/aulas/aula-13(repiticoes-p1)/script.js
@@ -0,0 +1,12 @@
+var cont = 1
+
+while (cont <= 6){
+    console.log(`While Ola ${cont}`)
+    cont ++ // ++ almenta em 1
+}
+cont = 1
+
+do {
+    console.log(`Do ola ${cont}`)
+    cont ++ 
+} while (cont <= 6)
