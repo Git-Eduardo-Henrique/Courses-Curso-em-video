e09735d551224b3cfb498ac8171fd9d339ce7638
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-04 00:16:35 -0300

    cev-javascript-aula-14

---
+++ cev-javascript/aulas/aula-14(repeticoes-p2)/script.js
@@ -0,0 +1,7 @@
+// primeiro valor = inicio/ variavel de inicio
+// segundo valor = condicao
+// terceiro valor = almentar ou diminuir o valor
+
+for (var cont = 1; cont <= 5; cont ++){
+    console.log(`contador: ${cont}`)
+}
