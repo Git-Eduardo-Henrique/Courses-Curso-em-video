3f4d9075d2db3ca46978e4630c6b073fff96e8e3
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-06-19 23:20:46 -0300

    cev-python-ex-28

---
+++ cev-python3/mundo-01/aula-10/ex[28]-jogo-da-adivinhacao/main.py
@@ -0,0 +1,27 @@
+from random import randint
+from time import sleep
+
+escolhido = randint(0, 5)
+
+titulo = "Jogo da adivinhação"
+
+print(30 * "\033[31m=-=", "\033[m")
+print(f'{titulo:^90}')
+print(30 * "\033[31m=-=", "\033[m")
+
+digitado = int(input("Digite um valor entre 0 a 5: "))
+
+print(30 * "\033[31m=-=", "\033[m")
+
+print("Processando...")
+sleep(1.5)
+
+print(30 * "\033[31m=-=", "\033[m")
+if digitado > 5:
+    print('Numero invalido')
+elif digitado == escolhido:
+    print(f"Acertou!!! o valor é: {escolhido}")
+else:
+    print(f"Errou!!! voce digitou {digitado} e o valor é: {escolhido}")
+
+print(30 * "\033[31m=-=", "\033[m")
