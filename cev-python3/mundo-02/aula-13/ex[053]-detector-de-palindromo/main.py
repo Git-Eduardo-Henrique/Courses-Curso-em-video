e40bd01c4599b9577c640d227baa907851df1173
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-06-27 17:48:54 -0300

    cev-python-ex-53

--- cev-python3/mundo-02/aula-13/ex[053]-detector-de-palindromo/main.py
+++ cev-python3/mundo-02/aula-13/ex[053]-detector-de-palindromo/main.py
@@ -4,13 +4,16 @@ espaços. Exemplos de palíndromos:
 APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
 """
 print(30 * "\033[31m=-=", "\033[m")
-frase = str(input("digite uma frase: "))
-tam_frase = len(frase)
-nova_frase = ""
+
+frase = str(input("digite uma frase: ")).split(" ")
+nova_frase = ''.join(frase)
+reverso_frase = nova_frase[::-1]
+
 print(30 * "\033[31m=-=", "\033[m")
 
-for cont in range(1, tam_frase + 1):
-    nova_frase += frase[tam_frase - cont]
+if nova_frase == reverso_frase:
+    print("esta frase é um palindromo!")
+else:
+    print("esta frase não é um palindromo!")
 
-print(frase, nova_frase)
 print(30 * "\033[31m=-=", "\033[m")
